8a0f27888035287873230ee52c93b807ed1985df
fabmukunzi <mukunzifabrice77@gmail.com>
2024-04-28 13:48:52 +0200

    [feat]: send email fn

---
+++ src/utils/sendMail.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+const emailconfig = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  service: process.env.EMAIL_SERVICE,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export async function sendEmail({ to, subject, body }) {
+  const info = await emailconfig.sendMail({
+    from: `Posinnove <${process.env.EMAIL_USERNAME}>`,
+    to: to,
+    subject: subject,
+    html: body,
+  });
+
+  return info;
+}
