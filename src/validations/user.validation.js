3c5e288308c27afc10a92abbf0f64c9e0b6a9333
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2024-06-17 17:34:58 +0200

    [fix]: return validation error

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,45 +1,50 @@
 import Joi from 'joi';
 
 const usersValidation = Joi.object({
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-      .required()
-      .messages({ 'string.email': 'Please enter a valid email address' }),
-    password: Joi.string()
-      .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
-      .required()
-      .messages({ 'string.pattern.base': 'Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long.' }),
-    firstName: Joi.string()
-      .min(3)
-      .max(20)
-      .required()
-      .messages({ 'string.min': 'First name is required' }),
-    lastName: Joi.string()
-      .min(3)
-      .max(20)
-      .required()
-      .messages({ 'string.min': 'Last name is required' }),
-    gender: Joi.string()
-      .min(3)
-      .max(20)
-      .optional()
-      .messages({ 'string.min': 'Gender is required' }),
-    role: Joi.string()
-      .valid('student', 'teacher')
-      .required()
-      .messages({ 'any.only': 'Role must be either "teacher" or "student"' }),
-  });
- 
- const validateUser = (req, res, next) => {
-    const { error } = usersValidation.validate(req.body, { abortEarly: false });
-    if (error) {
-        return res.status(400).json({
-            status: "fail",
-            data: {
-                message: error.details[0].message
-            }
-        });
-    }
-    next();
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required()
+    .messages({ 'string.email': 'Please enter a valid email address' }),
+  password: Joi.string()
+    .pattern(
+      new RegExp(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+      )
+    )
+    .required()
+    .messages({
+      'string.pattern.base':
+        'Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long.',
+    }),
+  firstName: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ 'string.min': 'First name is required' }),
+  lastName: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ 'string.min': 'Last name is required' }),
+  gender: Joi.string()
+    .min(3)
+    .max(20)
+    .optional()
+    .messages({ 'string.min': 'Gender is required' }),
+  role: Joi.string()
+    .valid('student', 'teacher')
+    .required()
+    .messages({ 'any.only': 'Role must be either "teacher" or "student"' }),
+});
+
+const validateUser = (req, res, next) => {
+  const { error } = usersValidation.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      status: 'fail',
+      message: error.details[0].message,
+    });
+  }
+  next();
 };
-export default validateUser
+export default validateUser;
