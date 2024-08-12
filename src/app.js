d117ab46d5195852d937a522d1c454f6b700cb49
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2024-08-12 15:54:02 +0200

    [fix]: added custom css file

--- src/app.js
+++ src/app.js
@@ -10,18 +10,26 @@ import docs from './documentation';
 // const swaggerDocument = require('./swagger.json');
 import swaggerUi from 'swagger-ui-express';
 
-
+const CSS_URL =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 const app = express();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(docs));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(docs, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+  })
+);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.get('/', (req, res) => { return res.status(200).json({ message: 'welcome to posinnove Backend APIs'});
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'welcome to posinnove Backend APIs' });
 });
 app.use('/api/users', userRoutes);
 app.use('/api/categories', projectCategory);
-app.use("/api/projects",projectRoutes)
-
-
+app.use('/api/projects', projectRoutes);
 
 export default app;
--- src/documentation/basicInfo.js
+++ src/documentation/basicInfo.js
@@ -1,60 +1,61 @@
+import { config } from 'dotenv';
+
+config();
 const basicInfo = {
-    openapi: "3.0.0",
-    info: {
-      title: "Posinnove API",
-      version: "1.0.0",
-      description: "This is the APIs documentation for Posinnove Platform"
+  openapi: '3.0.0',
+  info: {
+    title: 'Posinnove API',
+    version: '1.0.0',
+    description: 'This is the APIs documentation for Posinnove Platform',
+  },
+  // servers: [
+  //   {
+  //     url:
+  //       process.env.NODE_ENV === 'development'
+  //         ? 'http://localhost:5000'
+  //         : 'https://posinnove-be.vercel.app/',
+  //   },
+  // ],
+  tags: [
+    {
+      name: 'Welcome',
+      description: 'Welcome endpoints for Posinnove Backend',
     },
-    servers: [
-      {
-        url: "http://localhost:5000",
-        description: "Development server"
-      },
-      {
-        url: "https://posinnove-be.vercel.app/",
-        description: "Production server (HTTPS)"
-      }
-    ],
-    tags: [
-      {
-        name: "Welcome",
-        description: "Welcome endpoints for Posinnove Backend"
-      },
-      {
-        name: "Authentication",
-        description: "APIs for managing Authentication"
-      },
-      {
-        name: "Profile",
-        description: "APIs for managing Profile"
-      },
-      {
-        name: "Users",
-        description: "APIs for managing Users"
-      },
-      {
-        name: "Project Categories",
-        description: "APIs for managing Project Categories"
-      },{
-        name: "Project",
-        description: "APIs for managing Projects"
-      },
-      
-    ],
-    components: {
-        securitySchemes: {
-            Bearer: {
-              type: 'apiKey',
-              name: 'Authorization',
-              in: 'header'
-            }
-          }
+    {
+      name: 'Authentication',
+      description: 'APIs for managing Authentication',
     },
-    security: [
-      {
-        Bearer: []
-      }
-    ]
-  };
-  
-  export default basicInfo;
+    {
+      name: 'Profile',
+      description: 'APIs for managing Profile',
+    },
+    {
+      name: 'Users',
+      description: 'APIs for managing Users',
+    },
+    {
+      name: 'Project Categories',
+      description: 'APIs for managing Project Categories',
+    },
+    {
+      name: 'Project',
+      description: 'APIs for managing Projects',
+    },
+  ],
+  components: {
+    securitySchemes: {
+      Bearer: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+    },
+  },
+  security: [
+    {
+      Bearer: [],
+    },
+  ],
+};
+
+export default basicInfo;
